af71c771e2724e66793141011d19a5dc4a5b7433
Daniel Souza da Silva <dssilva@padtec.com.br>
2017-12-11 16:12:49 -0200

    inicio
    Change-Id: I7cb8f656f221b7497dcbdd0845a6be9b9192861e

---
+++ client/app/foto/foto.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'foto',
+    templateUrl: './app/foto/foto.component.html'
+})
+export class FotoComponent {
+    @Input() titulo: string = '';
+    @Input() url: string = '';
+    descricao: string = '';
+}
